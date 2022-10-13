27ad4d1dd0dd046b76f1258f12fd0c68e58fc437
Robert Östberg <robbanost@gmail.com>
2022-10-13 20:27:08 +0200

    test run

--- clicker.py
+++ clicker.py
@@ -1,9 +1,15 @@
 import time
 from pynput.keyboard import Key, Controller
-from pynput.mouse import Controller, Button
+from pynput import mouse
 from pywinauto import Desktop
 
-wait = 1
+delay = 5
+wait = 3
+duration = 60
+targetN = -1
+target = "Minecraft "
+t = Controller()
+m = mouse.Controller()
 
 def pressKey(controller, key):
     controller.press(key)
@@ -11,15 +17,35 @@ def pressKey(controller, key):
 
 def tapKey(controller, key):
     controller.tap(key)
-    time.sleep(wait)
+    time.sleep(wait/20)
 
 def releaseKey(controller, key):
     controller.release(key)
     time.sleep(wait)
 
+def clickButton(controller, button):
+    controller.click(button)
+    time.sleep(wait)
+'''
 windows = Desktop(backend="uia").windows()  # creates a list of windows (and 4 other things)
-for w in windows:
-    print(w)
+
+for w in range(len(windows)):
+    text = windows[w].window_text()
+    #print(windows[w])
+    #for i in range(len(text)-len(target)):
+    if text[0:len(target)] == target:
+        targetN = w
+        print("yes, w blev:",w)
+windows[target].activate
 time.sleep(120)
 
-# Minecraft 1.19.2 - Multiplayer (LAN)
+# Minecraft 1.19.2 - Multiplayer (LAN)'''
+time.sleep(delay)
+for i in range(int(duration/wait)):
+    clickButton(m, mouse.Button.left)
+tapKey(t, Key.enter)
+tapKey(t, 'd')
+tapKey(t, 'o')
+tapKey(t, 'n')
+tapKey(t, 'e')
+tapKey(t, Key.enter) 
